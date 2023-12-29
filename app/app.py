e4a97d7d06d9af62b907e29a1d422829efd260f5
isgust <rocha.gustavops@gmail.com>
2023-12-29 01:34:25 -0300

    Rota de cadastro

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from database import conectar_banco, configurar_e_criar_tabelas
+from database import conectar_banco, configurar_e_criar_tabelas, cadastrar_usuario
 
 app = Flask(__name__)
 
@@ -30,8 +30,36 @@ def login():
     return render_template('login.html')
 
 #Rota de cadastro geral para todos usuários
-@app.route('/cadastro')
+@app.route('/cadastro', methods = ['GET', 'POST'])
 def cadastro():
+    if request.method == 'POST':
+        email = request.form['inputEmail4']
+        nick = request.form['inputNick']
+        nome = request.form['inputName']
+        sobrenome = request.form['inputSurname']
+        senha = request.form['inputPassword4']
+        tipo_usuario = request.form['userType']
+        cidade = request.form['inputCity']
+        bairro = request.form.get('inputBairro', '')  # Pode ser nulo, então usar get
+        estado = request.form['inputState']
+
+        # Se o tipo de usuário for fotógrafo, obter dados específicos do fotógrafo
+        dados_fotografo = None
+        if tipo_usuario == 'fotografo':
+        # Obter dados específicos do fotógrafo do formulário
+            dados_fotografo = (
+                request.form['inputCPF'],
+                request.form['inputProfilePic'],
+                request.form['inputContact'],
+                request.form['inputArea']
+            )
+
+        # Chamar a função cadastrar_usuario com os dados do formulário
+        mensagem = cadastrar_usuario(email, nick, nome, sobrenome, senha, tipo_usuario, cidade, bairro, estado, dados_fotografo)
+
+        return render_template('resultado_cadastro.html', mensagem=mensagem)
+
+
     return render_template('cadastro.html')
 
 
