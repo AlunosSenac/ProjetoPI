d1fd9431e4fc9c8a5784e9ac0fa6da626fa56a3b
Clodomilson <clodomilsonanjos.eng@outlook.com>
2024-05-23 10:10:22 -0300

    "fire auth"

--- flutter__app/lib/LoginPage.dart
+++ flutter__app/lib/LoginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter__app/footer_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatelessWidget {
   @override
